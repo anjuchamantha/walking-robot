71134f865982aa1bdd7ed3cd562d4fb9a7103cc1
anju97chamantha <chamantha97anju@gmail.com>
2018-08-31 18:40:23 +0530

    test 1

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,23 +1,23 @@
 #include <Servo.h>
-Servo servo1;
-Servo servo2;
+Servo servo1,servo2;
+//Servo servo2;
 
 void setup() {
   // put your setup code here, to run once:
   servo1.attach(9);
-  servo2.attach(9);
+  //servo1.write(100);
 }
 
 void loop() {
-  servo1.write(100);
-  delay(500);
+  servo1.write(110);
+  delay(20);
   servo1.write(90);
-  servo2.write(100);
-  delay(500);
-  servo2.write(90);
   delay(1000);
-  servo1.write(80);
-  delay(500);
+  servo1.write(70);
+  delay(20);
+  servo1.write(90);
+  delay(1000);
+  
   
   
 
